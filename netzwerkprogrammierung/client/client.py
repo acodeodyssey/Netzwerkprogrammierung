70e2f6103766cc7d11707195db2e646aab2383fc
Julian <jleichert@techfak.uni-bielefeld.de>
2018-06-19 11:49:08 +0200

    Socket Server/Client struktur aus Folien übernommen, closes #3

--- netzwerkprogrammierung/client/client.py
+++ netzwerkprogrammierung/client/client.py
@@ -1,1 +1,36 @@
-import os
+import socket
+import sys
+
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+cmd = None
+if len(sys.argv) >= 4:
+    cmd = sys.argv[3]
+
+# create new socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+
+# connect to host:port
+s.connect((host, port))
+
+# get information about connection
+local = s.getsockname()
+remote = s.getpeername()
+
+print("{}:{} -> {}:{}".format(local[0], local[1], remote[0], remote[1]))
+
+# send cmd
+if cmd is not None:
+    # convert cmd into byte array
+    sentbytes = s.send(bytes(cmd+"\n", 'utf-8'))
+
+# recieve response
+while True:
+    recievedbytes = s.recv(10)
+    if (len(recievedbytes) == 0):
+        break
+    print(recievedbytes.decode('utf-8'))
+
+# close connection
+s.close()
--- netzwerkprogrammierung/server/server.py
+++ netzwerkprogrammierung/server/server.py
@@ -1,1 +1,33 @@
-import os
+import socket
+import time
+
+
+host = 'localhost'
+port = 8443
+
+# create new socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+
+# bind to localhost/port
+s.bind((host, port))
+
+# listen on socket
+s.listen(socket.SOMAXCONN)
+
+print("Start server at {}:{}".format(host, port))
+
+try:
+    while True:
+        inSocket, addr = s.accept()
+
+        print("Connection from {}".format(addr))
+
+        msg = "Hello {0}, nice to meet you!\n".format(*socket.gethostbyaddr
+                                                      (addr[0]))
+        msg2 = "it's {1}/{2}/{0} {3}:{4}:{5}\n".format(*time.localtime())
+        inSocket.send(bytes(msg, 'utf-8'))
+        inSocket.send(bytes(msg2, 'utf-8'))
+        inSocket.close()
+
+finally:
+    s.close()
