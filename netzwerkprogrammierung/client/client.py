d8c83d5488fa325f91fb00411344ea3ca75cbf62
Julian <jleichert@techfak.uni-bielefeld.de>
2018-06-24 16:14:00 +0200

    ClientInfo created, refs #4

--- netzwerkprogrammierung/client/client.py
+++ netzwerkprogrammierung/client/client.py
@@ -1,7 +1,30 @@
 import uuid
 import socket
 import sys
-import os
+import platform
+import ruamel.yaml
+
+
+class ClientInfo:
+    yaml_tag = u'!clientInfo'
+
+    def __init__(self, machine, system, distro):
+        self.machine = machine
+        self.system = system
+        self.distro = distro
+
+    @classmethod
+    def to_yaml(cls, representer, node):
+        return representer.represent_scalar(cls.yaml_tag,
+                                            u'{.machine}-\
+                                            {.system}-\
+                                            {.distro}'.format(node,
+                                                              node,
+                                                              node))
+
+    @classmethod
+    def from_yaml(cls, constructor, node):
+        return cls(*node.value.split('-'))
 
 
 class MyClient:
@@ -9,15 +32,18 @@ class MyClient:
         # initialize socket
         if s is None:
             self.s = socket.socket(socket.AF_INET,
-                                   socket.SOCK_STREAM, socket.IPPROTO_TCP)
+                                   socket.SOCK_STREAM,
+                                   socket.IPPROTO_TCP)
         else:
             self.s = s
-        # initialize info and id
         self.host = sys.argv[1]
         self.port = int(sys.argv[2])
-        osinfo = os.uname()
-        id = uuid.uuid1()
-        self.info = [id, osinfo]
+        self.id = uuid.uuid1()
+        yaml = ruamel.yaml.YAML()
+        yaml.register_class(ClientInfo)
+        self.info = yaml.dump([ClientInfo(platform.machine(),
+                               platform.system(),
+                               platform.linux_distribution())], sys.stdout)
 
     def connect(self, host, port):
         self.s.connect((host, port))
@@ -25,6 +51,7 @@ class MyClient:
         self.remote = self.s.getpeername()
 
     def send(self):
+        cmd = None
         if len(sys.argv) >= 4:
             cmd = sys.argv[3]
         # send cmd
@@ -38,6 +65,15 @@ class MyClient:
             if (len(recievedbytes) == 0):
                 break
             print(recievedbytes.decode('utf-8'))
-
-    def disconnect(self):
         self.s.close()
+
+
+def main():
+    client = MyClient()
+    client.connect(client.host, client.port)
+    client.send()
+    client.receive()
+
+
+if __name__ == "__main__":
+    main()
