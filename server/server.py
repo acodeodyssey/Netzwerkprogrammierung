565ff2f585d5ca09c355bca8c6be40328e066013
Julian <jleichert@techfak.uni-bielefeld.de>
2018-07-06 13:48:27 +0200

    Threading, multiple clients can say hello, refs #7

--- server/server.py
+++ server/server.py
@@ -1,41 +1,47 @@
 import json
 import socket
-import time
 from threading import Thread
 
 
-class MyServer():
-    def __init__(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-        self.host = 'localhost'
-        self.clients = []
-        # bind to localhost/port
-        self.s.bind((self.host, 8443))
-        self.s.listen(socket.SOMAXCONN)
-        print("Start server at {}:{}".format(self.host, 80))
+class ClientHandler(Thread):
+    def __init__(self, inSocket, address):
+        Thread.__init__(self)
+        self.socket = inSocket
+        self.clientaddress = address
+        print("Connection from {}".format(address))
 
-    def acceptclients(self):
-        try:
-            while True:
-                inSocket, addr = self.s.accept()
-                print("Connection from {}".format(addr))
+    def run(self):
+        while True:
+            recievedbytes = inSocket.recv(1024)
+            if len(recievedbytes) == 0:
+                break
+            recievedmsg = recievedbytes.decode("utf-8")
+            data = json.loads(recievedmsg)
+            if data['type'] == "hello":
+                clients.append(data['content'])
+                print("Client with Id: " + data['content'][0]['id'] + " said hello")
+                self.send("hi client")
 
-        finally:
-            self.s.close()
+    def send(self, msg):
+        self.socket.send(str.encode(msg + "\n"))
 
-        def handleclient(self):
-            while True:
-                recievedbytes = inSocket.recv(1024)
-                if len(recievedbytes) == 0:
-                    break
-                recievedmsg = recievedbytes.decode("utf-8")
-                data = json.loads(recievedmsg)
-                if data['type'] == "hello":
-                    self.clients.append(data['content'])
-                    print("Client with Id: " + data['content'][0]['id'] + " said hello")
-            inSocket.send(str.encode("hello\n "))
-            inSocket.close()
+    def closesock(self):
+        self.socket.close()
+
 
 if __name__ == "__main__":
-    server = MyServer()
-    server.acceptclients()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    host = 'localhost'
+    clients = []
+    # bind to localhost/port
+    s.bind((host, 8443))
+    print("Start server at {}:{}".format(host, 8443))
+    try:
+        while True:
+            s.listen(socket.SOMAXCONN)
+            inSocket, addr = s.accept()
+            clientHandler = ClientHandler(inSocket, addr)
+            clientHandler.start()
+
+    finally:
+        s.close()
